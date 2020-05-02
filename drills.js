a10886f2442b28148c5475697fb88428e8d91280
Brenden Dowd <brenden.dowd@yahoo.com>
2020-05-02 12:53:39 -0700

    first

---
+++ drills.js
@@ -0,0 +1,197 @@
+// Merge Sort
+// Given the following list of numbers 21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40
+// third recursive call would be [ 1, 21 ]
+//16th call would be [ 16 ]
+//first merge [21][1]
+//7th merge [ 1, 21, 26, 45 ] [ 2, 9, 28, 29 ]
+
+function mSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  const middle = Math.floor(array.length / 2);
+  let left = array.slice(0, middle);
+  let right = array.slice(middle, array.length);
+
+  left = mSort(left);
+  right = mSort(right);
+  return merge(left, right, array);
+};
+
+function merge(left, right, array) {
+  let leftIndex = 0;
+  let rightIndex = 0;
+  let outputIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      array[outputIndex++] = left[leftIndex++];
+    }
+    else {
+      array[outputIndex++] = right[rightIndex++];
+    }
+  }
+
+  for (let i = leftIndex; i < left.length; i++) {
+    array[outputIndex++] = left[i];
+  }
+
+  for (let i = rightIndex; i < right.length; i++) {
+    array[outputIndex++] = right[i];
+  }
+  return array;
+};
+
+// quicksort
+//The pivot could have been either 14 or 17, both the values on either side are greater and less than respectively.
+//after the second partitioning using the last item as a pivot => [1,  3,  9, 22, 20, 17, 14]
+//after the second partitioning using the first item as a pivot => [9,  1,  3, 22, 20, 17, 14]
+
+function qSort(array, start = 0, end = array.length) {
+  if (start >= end) {
+    return array;
+  }
+  const middle = partition(array, start, end);
+  // console.log("middle",middle)
+
+  array = qSort(array, start, middle);
+  array = qSort(array, middle + 1, end);
+  return array;
+};
+function partition(array, start, end) {
+  const pivot = array[end - 1];
+
+  let j = start;
+  for (let i = start; i < end - 1; i++) {
+    if (array[i] <= pivot) {
+      swap(array, i, j);
+      j++;
+    }
+  }
+  swap(array, end - 1, j);
+  return j;
+};
+function swap(input, index_A, index_B) {
+  let temp = input[index_A];
+
+  input[index_A] = input[index_B];
+  input[index_B] = temp;
+}
+// console.log(
+//   qSort([89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5])
+// )
+// console.log(
+//   mSort([89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5])
+// )
+
+//6. Bucket Sort
+// InsertionSort to be used within bucket sort
+function insertionSort(array) {
+  var length = array.length;
+
+  for (var i = 1; i < length; i++) {
+    var temp = array[i];
+    for (var j = i - 1; j >= 0 && array[j] > temp; j--) {
+      array[j + 1] = array[j];
+    }
+    array[j + 1] = temp;
+  }
+
+  return array;
+}
+
+// Implement bucket sort
+function bucketSort(array, bucketSize) {
+  if (array.length === 0) {
+    return array;
+  }
+
+  // Declaring vars
+  var i,
+    minValue = array[0],
+    maxValue = array[0],
+    bucketSize = bucketSize || 5;
+
+  // Setting min and max values
+  array.forEach(function (currentVal) {
+    if (currentVal < minValue) {
+      minValue = currentVal;
+    } else if (currentVal > maxValue) {
+      maxValue = currentVal;
+    }
+  })
+
+  // Initializing buckets
+  var bucketCount = Math.floor((maxValue - minValue) / bucketSize) + 1;
+  var allBuckets = new Array(bucketCount);
+
+  for (i = 0; i < allBuckets.length; i++) {
+    allBuckets[i] = [];
+  }
+
+  // Pushing values to buckets
+  array.forEach(function (currentVal) {
+    allBuckets[Math.floor((currentVal - minValue) / bucketSize)].push(currentVal);
+  });
+
+  // Sorting buckets
+  array.length = 0;
+
+  allBuckets.forEach(function (bucket) {
+    insertionSort(bucket);
+    bucket.forEach(function (element) {
+      array.push(element)
+    });
+  });
+
+  return array;
+}
+
+// console.log(
+//   bucketSort([89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5])
+// )
+
+//7. Sort in place
+function selectionSort(arr){
+  let len = arr.length;
+  for (let i = 0; i < len; i++) {
+      let min = i;
+      for (let j = i + 1; j < len; j++) {
+          if (arr[min] > arr[j]) {
+              min = j;
+          }
+      }
+      if (min !== i) {
+        let tmp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = tmp;
+      }
+  }
+  return arr;
+}
+
+// console.log(
+//   selectionSort([89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5])
+// )
+
+//8. Sorting Books
+//given an array of book names, return them in alphabetical order
+function bookSort(arr){
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        let min = i;
+        for (let j = i + 1; j < len; j++) {
+            if (arr[min].charCodeAt([0]) > arr[j].charCodeAt([0])) {
+                min = j;
+            }
+        }
+        if (min !== i) {
+          let tmp = arr[i];
+          arr[i] = arr[min];
+          arr[min] = tmp;
+        }
+    }
+    return arr;
+}
+
+console.log(bookSort(['Perspolis Rising',"Catcher in the rye","Brave New Wold","1984", "Farenheit 451", "Great Gatsby", "Sapiens", "Game of Thrones", "Gardens of the Moon", "Tale of Two Cities", "It", "Feed", "Neuromancer", "Do Andriods Dream of Electric Sheep"]))
